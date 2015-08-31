99a43fd8f3ebdf1a3bb1e504c6838c03eca5dd07
Mewel <thegreatmewel@gmail.com>
2015-08-31 15:19:18 +0200

    fix missing spring-security-core dependency

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -20,20 +20,24 @@ grails.project.dependency.resolution = {
         grailsPlugins()
         grailsHome()
         grailsCentral()
-        mavenCentral()
+		mavenLocal()
+		mavenCentral()
+		mavenRepo "http://repo.grails.org/grails/repo/"
 
         // uncomment these to enable remote dependency resolution from public Maven repositories
-        //mavenCentral()
-        //mavenLocal()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+//		  mavenRepo "https://repo.grails.org/grails/plugins"
+//        mavenRepo "http://snapshots.repository.codehaus.org"
+//        mavenRepo "http://repository.codehaus.org"
+//        mavenRepo "http://download.java.net/maven/2/"
+//        mavenRepo "http://repository.jboss.com/maven2/"
+		
+        
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         runtime 'mysql:mysql-connector-java:5.1.21'
+		compile 'org.grails.plugins:spring-security-core:1.2.7.3'
     }
 
     plugins {
